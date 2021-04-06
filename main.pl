588a689e9fe621ded81e90b858a4ba8b50e1846e
Aidan <33972075+aid848@users.noreply.github.com>
2021-04-05 19:48:42 -0700

    Map generator created

--- main.pl
+++ main.pl
@@ -1,5 +1,10 @@
 :- use_module(library(pce)).
+:- include('map.pl').
 
+dims(X,Y) :- X is 500, Y is 500.
+mapSize(X,Y) :- X is 16, Y is 16.
+imgs(X,Y) :- X is 16, Y is 16.
+padding(T,B,L,R) :- T is 0, B is 50, L is 0, R is 50.
 % run the program with this
 run :- cleanup,init.
 
@@ -7,17 +12,20 @@ run :- cleanup,init.
 cleanup :- free(@bm).
 
 % game setup
-init :- new(P, picture('demo')),
+init :- 
+    new(MAINFRAME,frame('Minesweeper')),
+    new(P, picture),
+    send(MAINFRAME,append,P),
+    dims(A,B),
+    send(P,size,size(A,B)),
     send(P,open),
-    loadimg(P,T1,'32x32/books.xpm',25,25),
-    loadimg(P,T2,'16x16/print.xpm',50,50),
-    % funloop(30,30,P),
-    addPrologCallBack(T1,left,swapIcon,[T1,'16x16/print.xpm',P]),
-    addPrologCallBack(T2,left,swapIcon,[T2,'32x32/books.xpm',P]).
+    mapSize(MX,MY),
+    generateStartingState(MX,MY,MAP),
+    placeMap(P).
 
 
 % loads image into parent view
-loadimg(PARENT,IMG,PICTURE,X,Y) :- send(PARENT, display, new(IMG,bitmap(PICTURE)), point(X,Y)).
+loadimg(PARENT,IMG,PICTURE,X,Y) :- send(PARENT, display, new(IMG,bitmap(PICTURE)), point(X,Y)), send(IMG,size,size(32,32)).
 
 % message(target, callback, arg1,args2,...)
 addPrologCallBack(TARGET, CLICK, FUNCTION, ARGS) :- send(TARGET, recogniser,click_gesture(CLICK, '', single, message(@prolog,FUNCTION,prolog(ARGS)))).
@@ -26,13 +34,14 @@ addPrologCallBack(TARGET, CLICK, FUNCTION, ARGS) :- send(TARGET, recogniser,clic
 getTilePos(Tile,X,Y) :- get(Tile,x,X),get(Tile,y,Y), print(X).
 
 % removed an xpce bitmap and draws a new one in the same place as the old one based on the path to an acceptable image
-swapIcon([OLD,NEW,P]) :- getTilePos(OLD,X,Y),free(OLD), send(P,display,new(I,bitmap(NEW)), point(X,Y)), addToMap(I,X,Y).
+swapIcon([OLD,NEW,P]) :- getTilePos(OLD,X,Y),free(OLD), send(P,display,new(I,bitmap(NEW)), point(X,Y)).
 
-% funloop(_,0,0).
-% funloop(X,Y,P) :- loadimg(P,_,'32x32/books.xpm',X,Y), funloop(X - 5,Y - 5,P), X1 is X-5,Y1 is Y-5.
+placeMap(P) :- placeMapHelper(P,1,1).
+placeMapHelper(P,X,Y) :- mapSize(_,MY),Y=<MY, Y1 is Y+1,placeMapHelperRow(P,X,Y),print(Y),placeMapHelper(P,X,Y1).
 
-% TODO add tile reference to map rep *TODO
-% addToMap(Tile,X,Y).
+% TODO change the callback to the interaction handler AND add right click handler
+placeMapHelperRow(P,X,Y) :- mapSize(MAXX,_),X<MAXX,X1 is X + 1,mapToGrid(X,Y,A,B),loadimg(P,I,'./icons/unmarked.xpm',A,B),addPrologCallBack(I,left,swapIcon,[I,'16x16/print.xpm',P]), placeMapHelperRow(P,X1,Y). %getTile(X,Y,MAP,T)
+placeMapHelperRow(P,X,Y) :- mapSize(MAXX,_),X=:=MAXX,mapToGrid(X,Y,A,B),loadimg(P,I,'./icons/unmarked.xpm',A,B),addPrologCallBack(I,left,swapIcon,[I,'16x16/print.xpm',P]).
 
-% TODO map the internal coords to screen space
-% mapToGrid(X1,Y1,X2,Y2).
+% Map the internal coords to screen space (xpm images are 16x16)
+mapToGrid(X1,Y1,X2,Y2) :- imgs(H,W),padding(T,B,L,R),X2 is X1 * W + L, Y2 is Y1 * H + T.
