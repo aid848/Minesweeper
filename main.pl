f0038141dd237799f357f1f23cb372258367dd9f
Aidan <33972075+aid848@users.noreply.github.com>
2021-04-05 22:54:18 -0700

    added some more elements and foundation of score and timer elements. Restart beta functionality.

--- main.pl
+++ main.pl
@@ -1,24 +1,34 @@
 :- use_module(library(pce)).
 :- include('map.pl').
 
-dims(X,Y) :- X is 500, Y is 500.
+dims(X,Y) :- X is 400, Y is 400.
 mapSize(X,Y) :- X is 16, Y is 16.
 imgs(X,Y) :- X is 16, Y is 16.
-padding(T,B,L,R) :- T is 0, B is 50, L is 0, R is 50.
+padding(T,B,L,R) :- T is 50, B is 0, L is 75, R is 0.
 % run the program with this
 run :- cleanup,init.
 
 % Any named XPCE objects must be freed here, don't use named objects except for debug
-cleanup :- free(@bm).
+cleanup :- free(@c),free(@r),free(@s).
 
-% game setup
+% game setup TODO cleanup
 init :- 
     new(MAINFRAME,frame('Minesweeper')),
     new(P, picture),
     send(MAINFRAME,append,P),
     dims(A,B),
     send(P,size,size(A,B)),
+    send(P, display,new(@c, text(123)), point(300, 0)),
+    send(@c,font,font(helvetica, bold, 30)),
+    send(@c,colour,red),
+    send(P, display,new(@s, text(0)), point(100, 0)),
+    send(@s,font,font(helvetica, bold, 30)),
+    send(@s,colour,red),
+    send(P,display, new(@r,bitmap('./icons/smileybase.xpm')), point(200,0)),
+    addPrologCallBack(@r,left,restart,[P]),
     send(P,open),
+    % thread_create(countdown(X).),
+    % countdown(0),
     mapSize(MX,MY),
     generateStartingState(MX,MY,MAP),
     placeMap(P).
@@ -31,17 +41,23 @@ loadimg(PARENT,IMG,PICTURE,X,Y) :- send(PARENT, display, new(IMG,bitmap(PICTURE)
 addPrologCallBack(TARGET, CLICK, FUNCTION, ARGS) :- send(TARGET, recogniser,click_gesture(CLICK, '', single, message(@prolog,FUNCTION,prolog(ARGS)))).
 
 % gets both position coords from an xpce object
-getTilePos(Tile,X,Y) :- get(Tile,x,X),get(Tile,y,Y), print(X).
+getTilePos(Tile,X,Y) :- get(Tile,x,X),get(Tile,y,Y).
 
 % removed an xpce bitmap and draws a new one in the same place as the old one based on the path to an acceptable image
 swapIcon([OLD,NEW,P]) :- getTilePos(OLD,X,Y),free(OLD), send(P,display,new(I,bitmap(NEW)), point(X,Y)).
 
 placeMap(P) :- placeMapHelper(P,1,1).
-placeMapHelper(P,X,Y) :- mapSize(_,MY),Y=<MY, Y1 is Y+1,placeMapHelperRow(P,X,Y),print(Y),placeMapHelper(P,X,Y1).
+placeMapHelper(P,X,Y) :- mapSize(_,MY),Y=<MY, Y1 is Y+1,placeMapHelperRow(P,X,Y),placeMapHelper(P,X,Y1).
 
-% TODO change the callback to the interaction handler AND add right click handler
-placeMapHelperRow(P,X,Y) :- mapSize(MAXX,_),X<MAXX,X1 is X + 1,mapToGrid(X,Y,A,B),loadimg(P,I,'./icons/unmarked.xpm',A,B),addPrologCallBack(I,left,swapIcon,[I,'16x16/print.xpm',P]), placeMapHelperRow(P,X1,Y). %getTile(X,Y,MAP,T)
-placeMapHelperRow(P,X,Y) :- mapSize(MAXX,_),X=:=MAXX,mapToGrid(X,Y,A,B),loadimg(P,I,'./icons/unmarked.xpm',A,B),addPrologCallBack(I,left,swapIcon,[I,'16x16/print.xpm',P]).
+% TODO change the callback to the interaction handlers 
+placeMapHelperRow(P,X,Y) :- mapSize(MAXX,_),X<MAXX,X1 is X + 1,mapToGrid(X,Y,A,B),loadimg(P,I,'./icons/unmarked.xpm',A,B),addPrologCallBack(I,left,swapIcon,[I,'./icons/down.xpm',P]),addPrologCallBack(I,right,swapIcon,[I,'./icons/flag.xpm',P]), placeMapHelperRow(P,X1,Y). 
+placeMapHelperRow(P,X,Y) :- mapSize(MAXX,_),X=:=MAXX,mapToGrid(X,Y,A,B),loadimg(P,I,'./icons/unmarked.xpm',A,B),addPrologCallBack(I,left,swapIcon,[I,'./icons/down.xpm',P]),addPrologCallBack(I,right,swapIcon,[I,'./icons/flag.xpm',P]).
 
 % Map the internal coords to screen space (xpm images are 16x16)
-mapToGrid(X1,Y1,X2,Y2) :- imgs(H,W),padding(T,B,L,R),X2 is X1 * W + L, Y2 is Y1 * H + T.
+mapToGrid(X1,Y1,X2,Y2) :- imgs(H,W),padding(T,B,L,R),X2 is (X1 - 1) * W + L, Y2 is (Y1 - 1) * H + T.
+
+% countdown timer, blocks so needs to be run on a separate thread :(
+countdown(X) :- send(@c,string,X), sleep(1),X1 is X + 1, countdown(X1).
+
+% works but looks bad, todo maybe keep frame and redo sub elements
+restart([P]) :- free(P), run.
