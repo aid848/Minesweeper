6205d2e2774db24eadd9146f5ce274f096d52190
Aidan <33972075+aid848@users.noreply.github.com>
2021-04-06 11:15:18 -0700

    digital clock groundwork

--- main.pl
+++ main.pl
@@ -1,10 +1,15 @@
 :- use_module(library(pce)).
 :- include('map.pl').
 
+% Game config
 dims(X,Y) :- X is 400, Y is 400.
 mapSize(X,Y) :- X is 16, Y is 16.
 imgs(X,Y) :- X is 16, Y is 16.
 padding(T,B,L,R) :- T is 50, B is 0, L is 75, R is 0.
+mines(N) :- N is 100.
+b_setVal(mark,"Hi").
+
+
 % run the program with this
 run :- cleanup,init.
 
@@ -28,9 +33,10 @@ init :-
     addPrologCallBack(@r,left,restart,[P]),
     send(P,open),
     % thread_create(countdown(X).),
-    % countdown(0),
+    minecounter(0,=,R),
     mapSize(MX,MY),
     generateStartingState(MX,MY,MAP),
+    % countdown(0),
     placeMap(P).
 
 
@@ -44,11 +50,13 @@ addPrologCallBack(TARGET, CLICK, FUNCTION, ARGS) :- send(TARGET, recogniser,clic
 getTilePos(Tile,X,Y) :- get(Tile,x,X),get(Tile,y,Y).
 
 % removed an xpce bitmap and draws a new one in the same place as the old one based on the path to an acceptable image
-swapIcon([OLD,NEW,P]) :- getTilePos(OLD,X,Y),free(OLD), send(P,display,new(I,bitmap(NEW)), point(X,Y)).
+swapIcon([OLD,NEW,P]) :- getTilePos(OLD,X,Y),free(OLD), send(P,display,new(I,bitmap(NEW)), point(X,Y)),b_setVal(mark,"E").
 
+% call to setup playable map, uses one indexed coords like the map representation
 placeMap(P) :- placeMapHelper(P,1,1).
 placeMapHelper(P,X,Y) :- mapSize(_,MY),Y=<MY, Y1 is Y+1,placeMapHelperRow(P,X,Y),placeMapHelper(P,X,Y1).
 
+% Places a map tile row
 % TODO change the callback to the interaction handlers 
 placeMapHelperRow(P,X,Y) :- mapSize(MAXX,_),X<MAXX,X1 is X + 1,mapToGrid(X,Y,A,B),loadimg(P,I,'./icons/unmarked.xpm',A,B),addPrologCallBack(I,left,swapIcon,[I,'./icons/down.xpm',P]),addPrologCallBack(I,right,swapIcon,[I,'./icons/flag.xpm',P]), placeMapHelperRow(P,X1,Y). 
 placeMapHelperRow(P,X,Y) :- mapSize(MAXX,_),X=:=MAXX,mapToGrid(X,Y,A,B),loadimg(P,I,'./icons/unmarked.xpm',A,B),addPrologCallBack(I,left,swapIcon,[I,'./icons/down.xpm',P]),addPrologCallBack(I,right,swapIcon,[I,'./icons/flag.xpm',P]).
@@ -61,3 +69,10 @@ countdown(X) :- send(@c,string,X), sleep(1),X1 is X + 1, countdown(X1).
 
 % works but looks bad, todo maybe keep frame and redo sub elements
 restart([P]) :- free(P), run.
+
+minecounter(X,+,R) :- R is X + 1.
+minecounter(X,-,R) :- R is X - 1.
+minecounter(X,=,R) :- R is X.
+
+% convert a number X into three position images for the gui
+digitalClock(X,A,B,C) :- C is X rem 100, B is X rem 10, A is X rem 1.
